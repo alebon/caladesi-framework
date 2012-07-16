ef9cb44765e1400866c14af996e9f108661881e8
Alexej Bondarenko <alexej.bondarenko@ersocon.net>
2012-07-16 18:41:36 +0200

    Refactored graphDB initialisation to Pooled GraphDB instead of private attribute

--- caladesi-orientdb/src/main/scala/net/caladesiframework/orientdb/graph/OrientGraphRepository.scala
+++ caladesi-orientdb/src/main/scala/net/caladesiframework/orientdb/graph/OrientGraphRepository.scala
@@ -19,7 +19,7 @@ package net.caladesiframework.orientdb.graph
 import entity.GraphEntity
 import net.caladesiframework.orientdb.repository.CRUDRepository
 import repository.{GraphRepository}
-import com.orientechnologies.orient.core.db.graph.OGraphDatabase
+import com.orientechnologies.orient.core.db.graph.{OGraphDatabasePool, OGraphDatabase}
 import com.orientechnologies.orient.core.record.impl.ODocument
 import net.caladesiframework.orientdb.field._
 import com.orientechnologies.orient.core.intent.OIntentMassiveInsert
@@ -34,9 +34,10 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
   extends GraphRepository[EntityType] with CRUDRepository[EntityType] {
 
   // @TODO Inject by configuration
-  private val graphDB = new OGraphDatabase("remote:127.0.0.1/db")
-  private val userName = "admin"
-  private val password = "admin"
+  def dbType = "remote"
+  def dbUser = "admin"
+  def dbName = "db"
+  def dbPassword = "admin"
 
   implicit def dbWrapper(db: OGraphDatabase) = new {
     def queryBySql[T](sql: String, params: AnyRef*): List[T] = {
@@ -52,34 +53,33 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
   /**
    * Creates the correct VertexType if missing
    */
-  def init = {
-    graphDB.open(userName, password)
-
-    graphDB.getVertexType(repositoryEntityClass) match {
+  def init = transactional (implicit db => {
+    db.getVertexType(repositoryEntityClass) match {
       case clazz: OClass => // Everything fine, no update needed
       case _ =>
-        graphDB.createVertexType(repositoryEntityClass, "OGraphVertex")
+        db.createVertexType(repositoryEntityClass, "OGraphVertex")
     }
+  })
 
-    graphDB.close()
-  }
-
-  def findByUuid(id: util.UUID): EntityType = {
-    graphDB.open(userName, password)
-
-    val result = graphDB.queryBySql("SELECT FROM " + repositoryEntityClass + " WHERE _uuid = '" + id.toString + "'")
+  /**
+   * Finds an entity by given uuid
+   *
+   * @param id
+   * @return
+   */
+  def findByUuid(id: util.UUID): EntityType = transactional(implicit db => {
+    val result = db.queryBySql("SELECT FROM " + repositoryEntityClass + " WHERE _uuid = '" + id.toString + "'")
 
     if (result.size == 0) {
       throw new Exception("Not found")
     }
     val document : ODocument = result.head
-    graphDB.close()
 
     val entity = this.create
     setEntityFields(entity, document)
 
     entity
-  }
+  })
 
   /**
    * Creates a new entity (not persisted)
@@ -98,14 +98,14 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
    */
   def update(entity: EntityType) = {
 
-      val vertex = transactional[ODocument](f => {
+      val vertex = transactional[ODocument](implicit db => {
         // Check for right VertexType present
-        graphDB.getVertexType(repositoryEntityClass) match {
+        db.getVertexType(repositoryEntityClass) match {
           case null => throw new Exception("Please run repository initialization before updating entities")
           case clazz: OClass => // Everything is fine
         }
 
-        val vertex = graphDB.createVertex(repositoryEntityClass)
+        val vertex = db.createVertex(repositoryEntityClass)
         setVertexFields(vertex, entity)
         vertex.save
 
@@ -122,11 +122,11 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
    * @param list
    * @return
    */
-  def update(list: List[EntityType]) = transactional(f => {
-    graphDB.declareIntent(new OIntentMassiveInsert())
+  def update(list: List[EntityType]) = transactional(implicit db => {
+    db.declareIntent(new OIntentMassiveInsert())
 
     // Check for right VertexType present
-    graphDB.getVertexType(repositoryEntityClass) match {
+    db.getVertexType(repositoryEntityClass) match {
       case null => throw new Exception("Please run repository initialization before updating entities")
       case clazz: OClass => // Everything is fine
     }
@@ -134,7 +134,7 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
     var vertex : ODocument = null
     list foreach {
       entity => {
-        vertex = graphDB.createVertex(repositoryEntityClass)
+        vertex = db.createVertex(repositoryEntityClass)
         setVertexFields(vertex, entity)
 
         vertex.save
@@ -142,7 +142,7 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
       }
     }
 
-    graphDB.declareIntent(null)
+    db.declareIntent(null)
 
     list
   })
@@ -153,13 +153,13 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
    * @param entity
    * @return
    */
-  def delete(entity: EntityType) = transactional[Boolean]( f => {
-    val result = graphDB.queryBySql("SELECT FROM " + repositoryEntityClass + " WHERE _uuid = '" + entity.uuid.is.toString + "'")
+  def delete(entity: EntityType) = transactional[Boolean]( implicit db => {
+    val result = db.queryBySql("SELECT FROM " + repositoryEntityClass + " WHERE _uuid = '" + entity.uuid.is.toString + "'")
 
     if (result.size == 0) {
       throw new Exception("Not found vertex with given uuid")
     }
-    graphDB.removeVertex(result.head)
+    db.removeVertex(result.head)
     true
   })
 
@@ -168,10 +168,10 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
    *
    * @return
    */
-  def count = transactional(f => {
+  def count = transactional(implicit db => {
     var count : Long = 0
 
-    val result = graphDB.queryBySql[Long]("SELECT COUNT(*) FROM " + repositoryEntityClass)
+    val result = db.queryBySql[Long]("SELECT COUNT(*) FROM " + repositoryEntityClass)
     count = result.head.toString.replace("}", "").replace("{", "").split(":").last.toLong
 
     count
@@ -179,22 +179,6 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
 
   def drop = {
     throw new Exception("Not implemented yet")
-  }
-
-  /**
-   * Open db
-   *
-   * @return
-   */
-  private def open() = {
-    graphDB.open(userName, password)
-  }
-
-  /**
-   * Close DB connection
-   */
-  private def close() = {
-    graphDB.close()
   }
 
   /**
@@ -257,22 +241,24 @@ abstract class OrientGraphRepository[EntityType <: GraphEntity] (implicit m:scal
    * @tparam T
    * @return
    */
-  def transactional[T <: Any](f: Any => T) : T = {
-    try {
-      graphDB.open(userName, password)
-      val transaction = synchronized { graphDB.begin() }
+  def transactional[T <: Any](f: OGraphDatabase => T) : T = {
+    val db = OGraphDatabasePool.global()
+      .acquire(dbType + ":127.0.0.1/" + dbName, dbUser, dbPassword)
 
-      val ret = f()
+    try {
+      val transaction = synchronized { db.begin() }
+
+      val ret = f(db)
 
       transaction.commit()
 
       return ret
     } catch {
       case e:Exception =>
-        graphDB.rollback()
+        db.rollback()
         throw new Exception("Failure during execution: " + e.getMessage)
     } finally {
-      graphDB.close()
+      db.close()
     }
   }
 
